bb736aca69d979a224f0fbdcac005b61846f65f7
Renato Luiz Carneiro Junior <rejr8@hotmail.com>
2018-02-17 02:01:39 -0500

    Lab 3

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,10 +6,16 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     public float maxSpeed = 10.0f;
+    public Transform groundCheck;
+    public LayerMask defineGround;
+    public float jumpForce = 5;
 
     private Rigidbody2D rBody;
     private SpriteRenderer sRend;
     private Animator animator;
+
+    private float groundCheckRadius = 0.3f;
+    private bool isGrounded = false;
 
 	// Use this for initialization
 	void Start () {
@@ -17,9 +23,26 @@ public class PlayerController : MonoBehaviour {
         sRend = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
     }
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+
+    private void Update()
+    {
+        if (Input.GetAxis("Jump") > 0 && isGrounded)
+        {
+            animator.SetBool("Ground", false);
+            rBody.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, defineGround);
+        //Debug.Log("Grounded?" + isGrounded);
+
+        animator.SetBool("Ground", isGrounded);
+
+        //Pass vertical velocity to animator
+        animator.SetFloat("vSpeed", rBody.velocity.y);
+
         float moveHoriz = Input.GetAxis("Horizontal");
 
         //Debug.Log("Horizontal value = " + moveHoriz);
