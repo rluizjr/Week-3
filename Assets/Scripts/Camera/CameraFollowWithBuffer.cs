94796816fdf022922f05188cc81d6b7418533e21
Renato Luiz Carneiro Junior <rejr8@hotmail.com>
2018-02-25 19:17:04 -0500

    Week 5 code

---
+++ Assets/Scripts/Camera/CameraFollowWithBuffer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowWithBuffer : MonoBehaviour {
+
+    public Transform playerPosition;
+    public Transform playerMoveThreshold;
+
+	// Use this for initialization
+	void Start () {
+        playerMoveThreshold = transform.GetChild(0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (playerPosition.position.x > playerMoveThreshold.position.x)
+        {
+            this.transform.position = new Vector3(playerPosition.position.x, transform.position.y, transform.position.z);
+        }
+	}
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(playerMoveThreshold.position, new Vector3(playerMoveThreshold.position.x, playerMoveThreshold.position.y + 100, playerMoveThreshold.position.z));
+        Gizmos.DrawLine(playerMoveThreshold.position, new Vector3(playerMoveThreshold.position.x, playerMoveThreshold.position.y - 100, playerMoveThreshold.position.z));
+    }
+}
---
+++ Assets/Scripts/Camera/CamraCloseUpTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamraCloseUpTrigger : MonoBehaviour {
+
+    public Camera mainCamera;
+    public Camera closeUpCamera;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            mainCamera.enabled = false;
+            closeUpCamera.enabled = true;
+        }   
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            mainCamera.enabled = true;
+            closeUpCamera.enabled = false;
+        }
+    }
+}
